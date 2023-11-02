916c9070d18ed1fe2fdee7f250338309a31de8d7
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-11-02 11:42:52 +0200

    Create keygen for encoding URl

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, json, request, jsonify
-
+import keygen
 
 app = Flask(__name__)
 
 
 @app.route('/shortener', methods=['POST'])
 def get_url():
-
-    return jsonify({"success": "true"})
+    data = request.json
+    url = data.get("url")
+    url_hash = keygen.create_random_key()
+    return jsonify({"success": "true",
+                    "initial_url": url,
+                    "new_url": url_hash})
 
 
 if __name__ == '__main__':
