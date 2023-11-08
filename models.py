ad64ad579e9fb0bbf8f3f0e8ed0b87adada1fa73
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-11-08 22:44:01 +0200

    Change gitingore file

--- models.py
+++ models.py
@@ -11,3 +11,6 @@ class Url(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key = True)
     initial_url: Mapped[str] = mapped_column(String(255), nullable=False)
     shortened_url: Mapped[str] = mapped_column(String(10), nullable=False)
+
+
+
