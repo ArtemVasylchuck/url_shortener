95ba7b62fb1f530b7f88938c3b83f879a449219c
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-11-09 23:52:39 +0200

    Add minor fixes

--- keygen.py
+++ keygen.py
@@ -5,7 +5,3 @@ import string
 def create_random_key(length: int = 5) -> str:
     chars = string.ascii_uppercase + string.digits
     return "".join([secrets.choice(chars) for _ in range(length)])
-
-
-if __name__ == "__main__":
-    print(create_random_key())
