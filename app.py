460b400a4527a8c98f61eb1bfab87fd8f1546240
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-11-09 23:16:46 +0200

    Revert "Add redis connection and shortener logic"
    This reverts commit df6e53d97b702c4c8cff1d451314e3dac1b1479b.

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
-from flask import Flask, json, request, jsonify, redirect
-
+from flask import Flask, json, request, jsonify
 import keygen
-
-import redis
+from db_config import db
 
 app = Flask(__name__)
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///urls.db"
+
+db.init_app(app)
 
 @app.route('/shortener', methods=['POST'])
 def get_url():
     data = request.json
     url = data.get("url")
     url_hash = keygen.create_random_key()
-    r.set(url_hash, url)
     return jsonify({"success": "true",
                     "initial_url": url,
                     "new_url": url_hash})
 
 
-@app.route('/s/<uri>/', methods=['GET'])
-def redirect_url(uri):
-    url = r.get(uri)
-    print(url)
-    return redirect(url)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
