df6e53d97b702c4c8cff1d451314e3dac1b1479b
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-11-09 23:11:22 +0200

    Add redis connection and shortener logic

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, json, request, jsonify
+from flask import Flask, json, request, jsonify, redirect
+
 import keygen
-from db_config import db
+
+import redis
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///urls.db"
-
-db.init_app(app)
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 @app.route('/shortener', methods=['POST'])
 def get_url():
     data = request.json
     url = data.get("url")
     url_hash = keygen.create_random_key()
+    r.set(url_hash, url)
     return jsonify({"success": "true",
                     "initial_url": url,
                     "new_url": url_hash})
 
 
+@app.route('/s/<uri>/', methods=['GET'])
+def redirect_url(uri):
+    url = r.get(uri)
+    print(url)
+    return redirect(url)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
